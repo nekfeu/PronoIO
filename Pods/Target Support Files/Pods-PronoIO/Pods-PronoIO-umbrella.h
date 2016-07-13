5cca4b7aca3e96e59034f0d995f83c008862930b
empoci_k <empoci_k@epitech.eu>
2016-07-13 22:14:31 +0200

    PodsFiles

---
+++ Pods/Target Support Files/Pods-PronoIO/Pods-PronoIO-umbrella.h
@@ -0,0 +1,6 @@
+#import <UIKit/UIKit.h>
+
+
+FOUNDATION_EXPORT double Pods_PronoIOVersionNumber;
+FOUNDATION_EXPORT const unsigned char Pods_PronoIOVersionString[];
+
---
+++ Pods/Target Support Files/Pods-PronoIOTests/Pods-PronoIOTests-umbrella.h
@@ -0,0 +1,6 @@
+#import <UIKit/UIKit.h>
+
+
+FOUNDATION_EXPORT double Pods_PronoIOTestsVersionNumber;
+FOUNDATION_EXPORT const unsigned char Pods_PronoIOTestsVersionString[];
+
